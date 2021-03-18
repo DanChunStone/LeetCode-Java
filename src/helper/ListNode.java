4d18730f32010aa6f214bda5d95af937ebde416a
danchun <1912504224@qq.com>
2021-03-18 21:59:49 +0800

    Pro 92

--- src/helper/ListNode.java
+++ src/helper/ListNode.java
@@ -19,4 +19,33 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("ListNode{");
+
+        stringBuilder.append(val);
+        ListNode theNext = next;
+        while (theNext != null) {
+            stringBuilder.append(" -> ").append(theNext.val);
+            theNext = theNext.next;
+        }
+
+        stringBuilder.append("}");
+        return stringBuilder.toString();
+    }
+    
+    public static ListNode build(int[] values) {
+        if (values.length <= 0)
+            return null;
+        ListNode head = new ListNode(values[0]);
+        ListNode temp = head;
+        int index = 1;
+        while (index < values.length) {
+            temp.next = new ListNode(values[index]);
+            temp = temp.next;
+            index++;
+        }
+        return head;
+    }
 }
