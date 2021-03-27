8868add2cb4ab641573c591fd49ebde8b510f9f2
danchun <1912504224@qq.com>
2021-03-27 16:18:06 +0800

    Pro 61

---
+++ src/ProHot_100/Pro010_RegularExpressionMatching.java
@@ -0,0 +1,24 @@
+package ProHot_100;
+
+/**
+ * @author shilijun
+ * @date 2021/3/19 17:01
+ */
+public class Pro010_RegularExpressionMatching {
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * 给你一个字符串s和一个字符规律p，请你来实现一个支持 '.'和'*'的正则表达式匹配。
+     * '.' 匹配任意单个字符
+     * '*' 匹配零个或多个前面的那一个元素
+     * 所谓匹配，是要涵盖整个字符串s的，而不是部分字符串。
+     */
+    class Solution {
+//        public boolean isMatch(String s, String p) {
+//            s.split()
+//        }
+    }
+}
