f6a2d2aef0ba001663f5b78212dbaf4fca3a8f1d
danchun <1912504224@qq.com>
2021-03-27 16:24:18 +0800

    add state

---
+++ src/Problem/Pro072_EditDistance.java
@@ -0,0 +1,17 @@
+package Problem;
+
+/**
+ * @author shilijun
+ * @date 2021/3/27 16:19
+ */
+public class Pro072_EditDistance {
+    public static void main(String[] args) {
+        System.out.println(new Solution().minDistance("intention", "execution"));
+    }
+
+    static class Solution {
+        public int minDistance(String word1, String word2) {
+            
+        }
+    }
+}
