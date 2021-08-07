576648203ec1f53eabbb0b0df44a1e602d75a88a
danchun <1912504224@qq.com>
2021-08-07 15:10:40 +0800

    resolve pro 34\39

--- src/ProHot_100/Pro034_FindFirstAndLastPosition.java
+++ src/ProHot_100/Pro034_FindFirstAndLastPosition.java
@@ -1,5 +1,7 @@
 package ProHot_100;
 
+import java.util.Arrays;
+
 /**
  * @author shilijun
  * @date 2020/12/1 11:56
@@ -7,7 +9,9 @@ package ProHot_100;
 public class Pro034_FindFirstAndLastPosition {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(new Pro034_FindFirstAndLastPosition().searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+        System.out.println(Arrays.toString(new Pro034_FindFirstAndLastPosition().searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
+        System.out.println(Arrays.toString(new Pro034_FindFirstAndLastPosition().searchRange(new int[]{}, 0)));
     }
 
     /**
@@ -17,7 +21,102 @@ public class Pro034_FindFirstAndLastPosition {
      * 进阶：
      * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
      */
+//    public int[] searchRange(int[] nums, int target) {
+//        if (nums == null || nums.length == 0)
+//            return new int[]{-1, -1};
+//
+//        int[] result = new int[]{-1, -1};
+//
+//        int targetIndex = binarySearch(nums, 0, nums.length - 1, target);
+//        result[0] = result[1] = targetIndex;
+//
+//        if (targetIndex >= 0) {
+//            while (result[0] >= 0 && nums[result[0]] == target) {
+//                result[0]--;
+//            }
+//            while (result[1] < nums.length && nums[result[1]] == target) {
+//                result[1]++;
+//            }
+//            result[0]++;
+//            result[1]--;
+//        }
+//
+//        return result;
+//    }
+
+    private int binarySearch(int[] nums, int start, int end, int target) {
+        if (start >= end) {
+            return nums[start] == target ? start : -1;
+        }
+        if (end - start == 1) {
+            if (nums[start] == target)
+                return start;
+            else if (nums[end] == target)
+                return end;
+            else
+                return -1;
+        }
+        int center = (start + end) / 2;
+        if (nums[center] > target) {
+            return binarySearch(nums, start, center - 1, target);
+        } else if (nums[center] < target) {
+            return binarySearch(nums, center + 1, end, target);
+        } else {
+            return center;
+        }
+    }
+
     public int[] searchRange(int[] nums, int target) {
-        return null;
+        if (nums == null || nums.length == 0)
+            return new int[]{-1, -1};
+
+        int[] result = new int[]{-1, -1};
+
+        int start = binarySearchStart(nums, 0, nums.length - 1, target);
+        int end = binarySearchEnd(nums, 0, nums.length - 1, target);
+        result[0] = start;
+        result[1] = end;
+
+        return result;
+    }
+
+    private int binarySearchStart(int[] nums, int start, int end, int target) {
+        if (start >= end) {
+            return nums[start] == target ? start : -1;
+        }
+        if (end - start == 1) {
+            if (nums[start] == target)
+                return start;
+            else if (nums[end] == target)
+                return end;
+            else
+                return -1;
+        }
+        int center = (start + end) / 2;
+        if (nums[center] >= target) {
+            return binarySearchStart(nums, start, center, target);
+        } else {
+            return binarySearchStart(nums, center + 1, end, target);
+        }
+    }
+
+    private int binarySearchEnd(int[] nums, int start, int end, int target) {
+        if (start >= end) {
+            return nums[start] == target ? start : -1;
+        }
+        if (end - start == 1) {
+            if (nums[end] == target)
+                return end;
+            else if (nums[start] == target)
+                return start;
+            else
+                return -1;
+        }
+        int center = (start + end) / 2;
+        if (nums[center] > target) {
+            return binarySearchEnd(nums, start, center - 1, target);
+        } else {
+            return binarySearchEnd(nums, center, end, target);
+        }
     }
 }
