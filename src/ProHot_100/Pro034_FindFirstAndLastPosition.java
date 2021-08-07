feddd0a7eaedd5e90c7c6fbd627a8ce6b0609f2b
danchun <1912504224@qq.com>
2021-08-07 13:44:36 +0800

    add pro 34

--- src/ProHot_100/Pro034_FindFirstAndLastPosition.java
+++ src/ProHot_100/Pro034_FindFirstAndLastPosition.java
@@ -7,9 +7,16 @@ package ProHot_100;
 public class Pro034_FindFirstAndLastPosition {
 
     public static void main(String[] args) {
-        
+
     }
 
+    /**
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     * 如果数组中不存在目标值 target，返回 [-1, -1]。
+     * <p>
+     * 进阶：
+     * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+     */
     public int[] searchRange(int[] nums, int target) {
         return null;
     }
